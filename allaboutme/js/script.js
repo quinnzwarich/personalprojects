2cf7f12d997e3d5cbd045cbf46e30fdc1b915e95
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2021-07-20 02:57:36 -0400

    made a rough first version of the portfolio

--- allaboutme/js/script.js
+++ allaboutme/js/script.js
@@ -1,11 +1,41 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
-// Code goes here
+// do you want to keep going
+
+$(`#dywtkg`).on(`mouseenter`, function (event) {
+  $(this).attr(`src`, `assets/images/pencil.gif`)
+});
+
+$(`#dywtkg`).on(`mouseleave`, function (event) {
+  $(this).attr(`src`, `assets/images/apple.png`)
+});
+
+// the murder mystery
+
+$(`#tmm`).on(`mouseenter`, function (event) {
+  $(this).attr(`src`, `assets/images/tmm.gif`)
+});
+
+$(`#tmm`).on(`mouseleave`, function (event) {
+  $(this).attr(`src`, `assets/images/tmm.png`)
+});
+
+// lonely planet
+
+$(`#lp`).on(`mouseenter`, function (event) {
+  $(this).attr(`src`, `assets/images/planet.gif`)
+});
+
+$(`#lp`).on(`mouseleave`, function (event) {
+  $(this).attr(`src`, `assets/images/planetemblem.png`)
+});
+
+// how it isn't so
+
+$(`#hiis`).on(`mouseenter`, function (event) {
+  $(this).attr(`src`, `assets/images/crazydoor.gif`)
+});
+
+$(`#hiis`).on(`mouseleave`, function (event) {
+  $(this).attr(`src`, `assets/images/regulardoor.png`)
+});
